f508d4f8154c52cf7cebe581b540ba9822704d7f
wellingtonhiago <wellingtonhiago2.0@gmail.com>
2022-04-03 00:28:39 -0300

    Initial commit

---
+++ pythonAula/name.py
@@ -0,0 +1,4 @@
+name = input("Name: ")
+print("Hello, " + name)
+#ou
+print(f"Hello, {name}")
---
+++ pythonAula/sequences.py
@@ -0,0 +1,15 @@
+name = "Saitama"
+print(name[3]) # imprime 't'
+
+#lista de nomes
+names = ["Goku", "Seiya", "Saitama"]
+print(names)
+#Imprime a lista de nomes
+print(names[0])
+#Imprime 'Goku'
+
+coodinateX = 10.0
+coodinateY = 20.0
+
+#tuplas de coodinate
+coodinate = (10.0, 20.0)
